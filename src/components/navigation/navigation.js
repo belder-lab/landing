0c3a05ecaff47038a7cfcee273fde25fd311286e
DaniilFawkes <Loa986@gmail.com>
2023-04-04 16:27:10 +0300

    Navigation header

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Navigation = () => {
+    return (
+			<div>
+				<h1>Project Name</h1>
+				<nav>
+					<ul>
+						<li>Home</li>
+						<li>Pricing</li>
+						<li>Soolution</li>
+						<li>FAQ</li>
+					</ul>
+				</nav>
+				<button>Call to action</button>
+			</div>
+		);
+};
+
+export default Navigation;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,12 @@
 import * as React from "react"
-
-function Profile() {
-	return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
-}
-
+import Navigation from "../components/navigation/navigation"
 export default function Gallery() {
 	return (
-		<section>
-			<h1>Amazing scientists</h1>
-			<Profile />
-			<Profile />
-			<Profile />
-		</section>
+		<article>
+			<header>
+				<Navigation />
+			</header>
+		</article>
 	);
 }
 
